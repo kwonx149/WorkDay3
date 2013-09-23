2331d2e3d1380f0e6d95ec7d51d56a92357086e2
Junghun Kwon <kwonx149@umn.edu>
2013-09-23 15:30:32 -0500

    Workday3

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -4,5 +4,36 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
 
+        Dim num1, num2 As Double
+
+        'If ser did not type the numbers, eliminate him
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then
+            MessageBox.Show("Error: Please type a number")
+            Return
+        End If
+
+        num1 = CDbl(txtFirstNum.Text)
+        num2 = CDbl(txtSecondNum.Text)
+
+        'If user did not type the positive numbers, eliminate him
+        If num1 < 0 Or num2 < 0 Then
+            MessageBox.Show("Error: Please type a positive number")
+            Return
+        End If
+
+        'Sterile area
+        If (num1 > num2) Then
+            txtResult.Text = "Larger number is " & num1
+        ElseIf (num2 > num1) Then
+            txtResult.Text = "Larger number is " & num2
+        Else
+            txtResult.Text = "The two are equal."
+
+        End If
+        
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
